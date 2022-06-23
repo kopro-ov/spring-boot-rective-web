2e659602ef79cb8e66beb68cfa79555b87b2fea6
kopro-ov <koprodev@gmail.com>
2022-06-23 20:42:25 +0900

    Init spring-reactive-web project

---
+++ hacking-spring-boot-application/src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingSrpingBootApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringboot.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingSrpingBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackingSrpingBootApplication.class, args);
+    }
+
+}
