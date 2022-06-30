82b0de38279c8c4d696f81cb69e1283ec2f3e724
kopro-ov <koprodev@gmail.com>
2022-06-30 22:19:40 +0900

    post /add/{id}이면 장바구니 상품 담기

--- hacking-spring-boot-ch2-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/HomeController.java
+++ hacking-spring-boot-ch2-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.greglturnquist.hackingspringboot.reactive.controller;
 
 import com.greglturnquist.hackingspringboot.reactive.domain.Cart;
+import com.greglturnquist.hackingspringboot.reactive.domain.CartItem;
 import com.greglturnquist.hackingspringboot.reactive.repository.CartRepository;
 import com.greglturnquist.hackingspringboot.reactive.repository.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
@@ -26,5 +29,31 @@ public class HomeController {
                 .modelAttribute("cart", this.cartRepository.findById("My Cart")
                         .defaultIfEmpty(new Cart("My Chart"))).build());
     }
+
+    @PostMapping("/add/{id}")
+    Mono<String> add(@PathVariable String id) {
+        return this.cartRepository.findById("My Cart")
+                .defaultIfEmpty(new Cart("My Cart"))
+                .flatMap(cart -> cart.getCartItemList().stream()
+                        .filter(cartItem -> cartItem.getItem().getId().equals(id))
+                        .findAny()
+                        .map(cartItem -> {
+                            cartItem.increment();
+                            return Mono.just(cart);
+                        })
+                        .orElseGet(()-> {
+                            return this.itemRepository.findById(id)
+                                    .map(item -> new CartItem(item))
+                                    .map(cartItem -> {
+                                        cart.getCartItemList().add(cartItem);;
+                                        return cart;
+                                    });
+                        }))
+                .flatMap(cart -> this.cartRepository.save(cart))
+                .thenReturn("redirect:/");
+
+    }
+
+    ;
 
 }
