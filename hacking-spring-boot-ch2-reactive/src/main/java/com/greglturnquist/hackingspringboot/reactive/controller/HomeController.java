4e8ec3a01002499be74f0a6751d160720a5781b5
kopro-ov <koprodev@gmail.com>
2022-06-30 23:31:37 +0900

    컨트롤러 비즈니스 로직 서비스단으로 변경

--- hacking-spring-boot-ch2-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/HomeController.java
+++ hacking-spring-boot-ch2-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/HomeController.java
@@ -1,9 +1,10 @@
 package com.greglturnquist.hackingspringboot.reactive.controller;
 
 import com.greglturnquist.hackingspringboot.reactive.domain.Cart;
-import com.greglturnquist.hackingspringboot.reactive.domain.CartItem;
 import com.greglturnquist.hackingspringboot.reactive.repository.CartRepository;
 import com.greglturnquist.hackingspringboot.reactive.repository.ItemRepository;
+import com.greglturnquist.hackingspringboot.reactive.service.CartService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,48 +13,27 @@ import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    private ItemRepository itemRepository;
-    private CartRepository cartRepository;
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
 
-    public HomeController(ItemRepository itemRepository, CartRepository cartRepository) {
-        this.itemRepository = itemRepository;
-        this.cartRepository = cartRepository;
-    }
+    private final CartService cartService;
+
 
     @GetMapping
     Mono<Rendering> home() {
         return Mono.just(Rendering.view("home.html")
                 .modelAttribute("items", this.itemRepository.findAll())
                 .modelAttribute("cart", this.cartRepository.findById("My Cart")
-                        .defaultIfEmpty(new Cart("My Chart"))).build());
+                        .defaultIfEmpty(new Cart("My Cart"))).build());
     }
 
     @PostMapping("/add/{id}")
     Mono<String> add(@PathVariable String id) {
-        return this.cartRepository.findById("My Cart")
-                .defaultIfEmpty(new Cart("My Cart"))
-                .flatMap(cart -> cart.getCartItemList().stream()
-                        .filter(cartItem -> cartItem.getItem().getId().equals(id))
-                        .findAny()
-                        .map(cartItem -> {
-                            cartItem.increment();
-                            return Mono.just(cart);
-                        })
-                        .orElseGet(()-> {
-                            return this.itemRepository.findById(id)
-                                    .map(item -> new CartItem(item))
-                                    .map(cartItem -> {
-                                        cart.getCartItemList().add(cartItem);;
-                                        return cart;
-                                    });
-                        }))
-                .flatMap(cart -> this.cartRepository.save(cart))
+        return this.cartService.addToCart("My Cart", id)
                 .thenReturn("redirect:/");
-
     }
-
-    ;
 
 }
