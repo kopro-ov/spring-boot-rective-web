8bac0ddd95d41ad4af10611f9a56c97c85a1f7c2
kopro-ov <koprodev@gmail.com>
2022-07-10 22:01:01 +0900

    Feat 한 개의 item 조회

--- hacking-spring-boot-ch2-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/ApiItemController.java
+++ hacking-spring-boot-ch2-reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/ApiItemController.java
@@ -3,8 +3,10 @@ package com.greglturnquist.hackingspringboot.reactive.controller;
 import com.greglturnquist.hackingspringboot.reactive.domain.Item;
 import com.greglturnquist.hackingspringboot.reactive.repository.ItemRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ApiItemController {
@@ -20,4 +22,8 @@ public class ApiItemController {
         return this.repository.findAll();
     }
 
+    @GetMapping("/api/items/{id}")
+    Mono<Item> findOne(@PathVariable String id) {
+        return this.repository.findById(id);
+    }
 }
